268734749b3383ee35ebdb79a4a427d1dafabcd8
chxchel <axel13alvarado@gmail.com>
2025-04-04 19:22:28 -0600

    Add files via upload

---
+++ flores-amarillas-tres-main/anim.js
@@ -0,0 +1,72 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
+var lyricsData = [
+  { text: "TUS OJOS ME NOMINAN", time: -2 },
+  { text: "A QUERERTE", time: 2 },
+  { text: "Y YO QUIERO SER EL GANADOR", time: 7.35 },
+  { text: "PROBABLEMENTE ESTOY ", time: 14 },
+  { text: "IMAGINANDO DEMSIADO", time: 17.5 },
+  { text: "YA ME VI HACIENDO PLANES ENTRE TU FAMILIA Y YO", time: 23 },
+  { text: "QUE TU HERMANITA TE DIGA QUE SOY EL MEJOR", time: 26.15 },
+  { text: "PORQUE ESTOY", time: 29.15 },
+  { text: "ETERNAMENTE ENAMORADO", time: 34 },
+  { text: "QUIERO GRITARLE AL MUNDO ENTERO QUE CONOCÍ A LA MEJOR", time: 39 },
+  { text: "QUE POR TI SIGO SIENDO MI MEJOR VERSIÓN", time: 42.35 },
+  { text: "POR ESO SOY", time: 46 },
+  { text: "EL AFORTUNADO", time: 50 },
+  { text: "Where I'm now? I don't know why", time: 91 },
+  { text: "Nice butterflies in my hands", time: 97 },
+  { text: "Too much light for twilight", time: 104 },
+  { text: "In the mood for the flowers love", time: 108 },
+  { text: "At the time", time: 144 },
+  { text: "The whisper of birds", time: 148 },
+  { text: "Lonely before the sun cried", time: 153 },
+  { text: "Fell from the sky", time: 158 },
+  { text: "Like water drops", time: 164 },
+  { text: "Where I'm now? I don't know why", time: 169 },
+  { text: "Nice butterflies in my hands", time: 176 },
+  { text: "Too much light for twilight", time: 183 },
+  { text: "In the mood for the flowers", time: 188 },
+  { text: "Love.", time: 140 },
+];
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    // Calcula la opacidad basada en el tiempo en la línea actual
+    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    // Restablece la opacidad y el contenido si no hay una línea actual
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 1000);
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
